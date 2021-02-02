a9d02fe7358709c905d603abbcdc041a52f07cb9
stephen pierce <pierces@gmail.com>
2021-02-01 23:52:52 -0500

    fix date parsing and bugs from the 2021 update

--- covid cubic.R
+++ covid cubic.R
@@ -1,17 +1,19 @@
 #https://covidtracking.com/api/v1/us/daily.csv
 library(tidyverse)
+dateFormat = "%Y-%m-%dT24:00:00Z"
 a  = read_csv( 'https://covidtracking.com/api/v1/us/daily.csv',col_types= cols(
 #a  = read_csv( '/Users/steve/Downloads/daily(1).csv',col_types= cols(
   .default = col_double(),
   hash = col_character(),
-  dateChecked = col_datetime(format = ""),
+  dateChecked = col_datetime(format = dateFormat),
  # fips = col_logical(),
   date=col_datetime(),
-  lastModified = col_datetime(format="")
+  lastModified = col_datetime(format=dateFormat)
 ))
 
 predDate = as.POSIXct("2020-05-05")
-startDate = as.POSIXct("2020-05-01")
+startDate = as.POSIXct("2020-02-26")
+plotStartDate = as.POSIXct( "2020-04-01")
 plotEndDate = as.POSIXct( "2021-04-01")
 
 b <- filter(a, date < predDate & date > startDate)
@@ -23,6 +25,7 @@ p = ggplot(data = a) +
    geom_line( aes(date,deathIncrease)) + 
  
   geom_vline( aes(xintercept=predDate), linetype="dotted") + 
-  coord_cartesian(ylim=c(0,3500), xlim=c( min(a$date), plotEndDate))
+  coord_cartesian(ylim=c(0,4500), xlim=c( plotStartDate, plotEndDate))
 png( filename="out.png", width=800, height=600)
 print(p)
+dev.off()
